c734080cd04b770b3446625beb45a3037b0e126a
Botanicus <knava.bestvinensis@gmail.com>
2008-06-23 21:40:05 +0200

    Added tasks - loading and realoading fixtures in db.

--- lib/merb-fixtures/merbtasks.rb
+++ lib/merb-fixtures/merbtasks.rb
@@ -1,8 +1,15 @@
-namespace :db do
-  namespace :fixtures do
-    desc "Load all fixtures to the database"
-    task :load do
-      puts("Not implemented yet.")
+desc "Load the fixtures into database"
+task "dm:db:fixtures:load" => :merb_env do
+  Fixture.all.each do |fixture|
+    if fixture.save
+      puts "~ #{fixture.class} saved ..."
+    else
+      puts "#{fixture.class} couldn't be saved ..."
+      puts fixture.errors.each { |error| puts error }
     end
   end
 end
+
+# Reload tasks
+desc "Reload the fixtures from database. Warning: it recreate the database!"
+task "dm:db:fixtures:reload" => ["dm:db:automigrate", "dm:db:migrate:up", "dm:db:fixtures:load"]
