93af02c8b8f288308754133545aff864a33ad239
Botanicus <knava.bestvinensis@gmail.com>
2008-07-08 17:37:39 +0200

    Helpers improved

--- lib/merb-fixtures/shared/helpers.rb
+++ lib/merb-fixtures/shared/helpers.rb
@@ -3,11 +3,18 @@ module Merb
     module Helpers
       LOREM = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
       # lorem :type => "html", :count => 5
-      def lorem(params)
-        count = params[:count] || 1
-        count.times do
-          output.push(LOREM)
+      def lorem(params = Hash.new)
+        output = Array.new
+        default = { :count => 1, :type => "plain" }
+        params = params.merge(default)
+        params[:count].times do
+          if params[:type].to_s.eql?("html")
+            output.push("<p>#{LOREM}</p>")
+          else
+            output.push(LOREM)
+          end
         end
+        return output
       end
     end
   end
